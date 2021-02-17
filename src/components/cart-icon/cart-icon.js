fe8b4e2e934528157292cc772cbf6c02baa773de
Raouf Aissaoui <raouf@hotmail.com>
2021-02-17 14:19:31 +0000

    added cartIcon and cartDropdown components, and implimented cart reducer

---
+++ src/components/cart-icon/cart-icon.js
@@ -0,0 +1,19 @@
+// import { ReactComponent } from '*.svg';
+import React from 'react';
+import './cart-icon.scss';
+import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
+import { toggleCartHidden } from '../redux/cart/cart.actions';
+import { connect } from 'react-redux';
+
+
+const CartIcon = ({toggleCartHidden}) => {
+    return (<div className="cart-icon" onClick={toggleCartHidden}>
+        <ShoppingIcon className="shopping-icon"/>
+        <span className="item-count"></span>    
+    </div>)
+}
+
+const mapDispatchToProps = dispatch =>( {
+    toggleCartHidden: ()=> dispatch(toggleCartHidden())
+})
+export default connect(null, mapDispatchToProps)(CartIcon) ;
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,9 +3,11 @@ import { connect } from 'react-redux';
 import {Link} from 'react-router-dom';
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from '../../firebase/firebase.utils';
-
+import CartDropdown from '../cart-dropdown/cart-dropdown';
+import CartIcon from '../cart-icon/cart-icon';
 import './header.scss';
-const Header = ({currentUser}) => {
+
+const Header = ({currentUser, hidden}) => {
     return (
         <div className="header">
             <Link to="/" className="logo-container"> 
@@ -30,13 +32,18 @@ const Header = ({currentUser}) => {
                         </Link>
                     )
                 }
+                <CartIcon/>
             </div>
+            {   
+                hidden ? null : <CartDropdown/>
+            }
         </div>
     )
 }
 
-const mapStateToProps = state =>({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser},cart:  {hidden}}) =>({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
